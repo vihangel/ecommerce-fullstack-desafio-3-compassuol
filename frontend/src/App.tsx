80c0788d4360e6b78847653621d158bcf34e31ac
Angel <vitoria.angel@dengun.com>
2024-10-18 00:17:48 -0400

    feat: appbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,23 @@
 // src/App.tsx
 import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import AppBar from './components/AppBar';
 import ProductsPage from './pages/ProductsPage';
+import GlobalStyles from './styles/GlobalStyles';
+import { theme } from './styles/theme';
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<ProductsPage />} />
-        {/* Adicione mais rotas aqui conforme precisar */}
-      </Routes>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Router>
+        <AppBar />
+        <Routes>
+          <Route path="/" element={<ProductsPage />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 };
 
---
+++ frontend/src/styles/theme.ts
@@ -0,0 +1,28 @@
+/** @format */
+
+// src/styles/theme.ts
+
+export const theme = {
+  colors: {
+    primary: "#B88E2F",
+    secondary: "#2EC1AC",
+    accent: "#E97171",
+    background: "#F4F5F7",
+    text: "#333333", // Preto Suave (texto principal)
+    muted: "#9F9F9F", // Cinza Escuro (texto secundário)
+    linkHover: "#B88E2F",
+    white: "#FFFFFF",
+    black: "#000000",
+  },
+  spacing: {
+    iconSpacing: "45px",
+    linkSpacing: "75px",
+    containerPadding: "1.5rem",
+  },
+  fonts: {
+    main: "'Poppins', sans-serif",
+  },
+  border: {
+    default: "1px solid #eaeaea",
+  },
+};
